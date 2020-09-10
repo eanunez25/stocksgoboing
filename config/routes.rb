55ea26f3fe975b5cf8cd3bdd5c9d3d3f8fe75141
Elvin Nunez <eanunez25@gmail.com>
2020-09-10 18:42:41 -0500

    style signup and login

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
   resources :static_pages
 
   root 'static_pages#home'
+
+  devise_scope :user do
+    # register
+    get 'signup', to: 'devise/registrations#new'
+    # sessions
+    get 'login', to: 'devise/sessions#new'
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
+
 end
