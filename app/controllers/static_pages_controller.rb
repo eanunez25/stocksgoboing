b5fc40dc2c3f564d66fb71e26a3168272a7a9ed0
Elvin Nunez <eanunez25@gmail.com>
2020-09-11 16:40:34 -0500

    remove trade count

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,6 @@ class StaticPagesController < ApplicationController
     @short_puts = current_user.short_puts.all if user_signed_in?
     @bear_call_spreads = current_user.bear_call_spreads.all if user_signed_in?
     @short_calls = current_user.short_calls.all if user_signed_in?
-    @trade = @bull_put_spreads.size + @short_puts.size + @bear_call_spreads.size + @short_calls.size
   end
 
 end
