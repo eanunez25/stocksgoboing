6ec0ea30ddd59d728da5766840b58e0ce05f750b
Elvin Nunez <eanunez25@gmail.com>
2020-09-11 14:12:36 -0500

    fix count method deployment bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,7 +5,7 @@ class StaticPagesController < ApplicationController
     @short_puts = current_user.short_puts.all if user_signed_in?
     @bear_call_spreads = current_user.bear_call_spreads.all if user_signed_in?
     @short_calls = current_user.short_calls.all if user_signed_in?
-    @trade = @bull_put_spreads.count + @short_puts.count + @bear_call_spreads.count + @short_calls.count
+    @trade = @bull_put_spreads.size + @short_puts.size + @bear_call_spreads.size + @short_calls.size
   end
 
 end
