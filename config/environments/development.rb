e31ec1ee8f7e54c317245b2ee22fdce9f48787e9
Elvin Nunez <eanunez25@gmail.com>
2020-09-10 22:08:39 -0500

    add mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,24 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'stocksgoboing@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'https://stocksgoboing.herokuapp.com/',
+  user_name:            ENV["email"],
+  password:             ENV["password"],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
 end
