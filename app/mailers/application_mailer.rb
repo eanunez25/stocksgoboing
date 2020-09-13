c49ffd1d440b0dd49842e26e68cd8bd95075c5f8
Elvin Nunez <eanunez25@gmail.com>
2020-09-12 22:36:41 -0500

    email in development works

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "stocksgoboing@gmail.com"
   layout 'mailer'
+
+  def new_trade
+    @user = params[:user]
+    mail to: @user.email, subject: "New Trade"
+  end
+  
 end
--- app/mailers/bull_put_spread_mailer.rb
+++ app/mailers/bull_put_spread_mailer.rb
@@ -1,8 +1,5 @@
 class BullPutSpreadMailer < ApplicationMailer
 
-  def new_trade
-    @user = params[:user]
-    mail to: @user.email, subject: "New Trade"
-  end
+
 
 end
