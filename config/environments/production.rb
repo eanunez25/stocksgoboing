6a63c5e7c5425d96a2b02ba28f63153111dcba33
Elvin Nunez <eanunez25@gmail.com>
2020-09-13 00:59:25 -0500

    try sendgrid login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,8 +117,8 @@ Rails.application.configure do
     :address   => 'smtp.sendgrid.net',
     :port      => 587,
     :domain    => 'sendgrid.com',
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password  => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['EMAIL2'],
+    :password  => ENV['password'],
     :authentication => 'plain',
     :enable_starttls_auto => true 
   }
