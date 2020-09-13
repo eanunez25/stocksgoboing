01aa0c536d6bd5a800b5ff8beb77595fd15b44fb
Elvin Nunez <eanunez25@gmail.com>
2020-09-13 12:16:18 -0500

    update views for new bps email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,9 +2,10 @@ class ApplicationMailer < ActionMailer::Base
   default from: "stocksgoboing@gmail.com"
   layout 'mailer'
 
-  def new_trade
-    @user = params[:user]
+  def new_trade(user, trade)
+    @user = user
+    @trade = trade
     mail to: @user.email, subject: "New Trade"
   end
-  
+
 end
