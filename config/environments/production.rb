8eb21913f66aaaaa782ad26dd75a2c0d44c9652f
Elvin Nunez <eanunez25@gmail.com>
2020-09-13 00:09:19 -0500

    send email with gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,17 +112,15 @@ Rails.application.configure do
 
 
 
-   # Setup the mailer config
-   config.action_mailer.delivery_method = :smtp
-   config.action_mailer.perform_deliveries = true
-   config.action_mailer.smtp_settings = {
-     :user_name => ENV['SENDGRID_USERNAME'],
-     :password => ENV['SENDGRID_PASSWORD'],
-     :domain => 'heroku.com',
-     :address => 'smtp.sendgrid.net',
-     :port => 587,
-     :authentication => :plain,
-     :enable_starttls_auto => true
-   }
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['email'],
+   :password             => ENV['password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
 
 end
