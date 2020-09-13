d7fe736107039863bc81f4f46f110d8ca77c9839
Elvin Nunez <eanunez25@gmail.com>
2020-09-13 00:31:35 -0500

    email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,6 +117,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
+   :domain                => 'heroku.com'
    :user_name            => ENV['email'],
    :password             => ENV['password'],
    :authentication       => "plain",
