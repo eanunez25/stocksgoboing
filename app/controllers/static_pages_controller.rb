e042a1c52f45cc6d8e8f945b232b0fe9121ba9a3
Elvin Nunez <eanunez25@gmail.com>
2020-09-21 18:16:37 -0500

    order trades in home screen

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def home
-    @bull_put_spreads = current_user.bull_put_spreads.where('expiration > ?', Date.today) if user_signed_in? 
-    @short_puts = current_user.short_puts.where('expiration > ?', Date.today) if user_signed_in?
-    @bear_call_spreads = current_user.bear_call_spreads.where('expiration > ?', Date.today) if user_signed_in?
-    @short_calls = current_user.short_calls.where('expiration > ?', Date.today) if user_signed_in?
+    @bull_put_spreads = current_user.bull_put_spreads.where('expiration > ?', Date.today).order('created_at ASC') if user_signed_in? 
+    @short_puts = current_user.short_puts.where('expiration > ?', Date.today).order('created_at ASC') if user_signed_in?
+    @bear_call_spreads = current_user.bear_call_spreads.where('expiration > ?', Date.today).order('created_at ASC') if user_signed_in?
+    @short_calls = current_user.short_calls.where('expiration > ?', Date.today).order('created_at ASC') if user_signed_in?
   end
 
 end
